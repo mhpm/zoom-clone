44a5bed2a27af43f938ede56dfdf7eaa28c6ae48
Michelle Perez <michelle.perez@bldr.com>
2024-04-16 15:53:36 -0400

    🎉  zoom clone - base project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
+
+const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
+  return (
+    <main>
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
